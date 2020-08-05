309ae7def5a76817123dfabbdfc4908dd158387c
JShaikh <jelonmusk@gmail.com>
2020-08-05 20:42:37 +0530

    Updates

---
+++ precache-manifest.d70a2d8f2300e2d824e3c1dca79eeaaf.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a49f4fb750cbb3ea10ea10592e6c78d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33a7fa90993b8a23ce3a",
+    "url": "/static/css/3.b499fcd2.chunk.css"
+  },
+  {
+    "revision": "20dd1ef6082fceac0a5c",
+    "url": "/static/css/4.01c87591.chunk.css"
+  },
+  {
+    "revision": "5e990a8bb1492aa23693",
+    "url": "/static/css/main.d69136ba.chunk.css"
+  },
+  {
+    "revision": "ab6e9f3b8e19805f35ad",
+    "url": "/static/js/2.329decc4.chunk.js"
+  },
+  {
+    "revision": "71772b085033e92f89cdfb3071333458",
+    "url": "/static/js/2.329decc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33a7fa90993b8a23ce3a",
+    "url": "/static/js/3.e59a6b5c.chunk.js"
+  },
+  {
+    "revision": "20dd1ef6082fceac0a5c",
+    "url": "/static/js/4.0c35962b.chunk.js"
+  },
+  {
+    "revision": "5e990a8bb1492aa23693",
+    "url": "/static/js/main.e61bfea5.chunk.js"
+  },
+  {
+    "revision": "491f5ba400505e1d1d1a",
+    "url": "/static/js/runtime-main.0f284b2f.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "2a7a8d38e530a23573fd6ebec7d36630",
+    "url": "/static/media/analyse.2a7a8d38.svg"
+  },
+  {
+    "revision": "1d1593a0352c4105855502d21fe98529",
+    "url": "/static/media/contact.1d1593a0.svg"
+  },
+  {
+    "revision": "922c1ed661f7d32c0d643038150379b1",
+    "url": "/static/media/devFocus.922c1ed6.svg"
+  },
+  {
+    "revision": "60692fda0d6641d7089524d845d8a9a8",
+    "url": "/static/media/girlSitting.60692fda.svg"
+  },
+  {
+    "revision": "3777510a614e2227474c4ec332762e27",
+    "url": "/static/media/imun.3777510a.jpeg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "de3da8064f2e9ffab945772c2c67e46f",
+    "url": "/static/media/talksCardBack.de3da806.svg"
+  }
+]);
